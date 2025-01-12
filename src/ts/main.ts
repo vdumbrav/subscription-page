995c153a4daf52e4c6d213a048abf0c11c010178
Vlad Dumbava <vladislav.dumbrava@gmail.com>
2025-01-12 21:35:26 +0200

    feat(pricing): add pricing dot and update card selection logic

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,8 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('.btn');
-    buttons.forEach((button) => {
-        button.addEventListener('click', () => {
-            alert('Subscription selected!');
-        });
+document.addEventListener("DOMContentLoaded", () => {
+  const pricingCards =
+    document.querySelectorAll<HTMLDivElement>(".pricing__card");
+
+  if (pricingCards[1]) {
+    pricingCards[1].classList.add("pricing__card--selected");
+  }
+
+  pricingCards.forEach((card, index) => {
+    card.addEventListener("click", () => {
+      pricingCards.forEach((c) =>
+        c.classList.remove("pricing__card--selected")
+      );
+
+      card.classList.add("pricing__card--selected");
+
+    //   setTimeout(() => {
+    //     alert(`Subscription ${index + 1} selected!`);
+    //   }, 300);
     });
+  });
 });
