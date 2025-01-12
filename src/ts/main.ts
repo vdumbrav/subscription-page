083e085962c164b45f535380c93987f9504f378a
Vlad Dumbava <vladislav.dumbrava@gmail.com>
2025-01-12 18:29:43 +0200

    Initial commit

---
+++ src/ts/main.ts
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.btn');
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            alert('Subscription selected!');
+        });
+    });
+});
