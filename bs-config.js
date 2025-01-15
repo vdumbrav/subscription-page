e064d32be289f5fee49dac2422e2154b5bc73f8e
Vlad Dumbava <vladislav.dumbrava@gmail.com>
2025-01-15 16:40:31 +0200

    feat(sign-up): add sign-up page and related styles and scripts

--- bs-config.js
+++ bs-config.js
@@ -1,6 +1,11 @@
 module.exports = {
   server: {
     baseDir: "./dist",
+    "routes": {
+      "/signup": "dist/signup.html"
+    }
   },
   files: ["./dist/**/*"], // Watch for changes in the dist directory
 };
+
+
